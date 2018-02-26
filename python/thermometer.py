8c0d0580dfb06fed5d3eea43454fcaca761198e6
Mark Cantrill <mark@astro-designs.com>
2018-02-26 19:46:05 +0000

    LM75 module

--- python/thermometer.py
+++ python/thermometer.py
@@ -17,13 +17,18 @@ Press Ctrl+C to exit.
 delay = 1
 sensor = LM75.LM75()
 
-while True:
-    clear()
-    temp_raw = sensor.getTemp()
-    temp = temp_raw
-    if temp > 128:
-        temp = temp - 256
-    print temp
-    write_string( "%.1f" % temp + " c", kerning=False)
-    show()
-    time.sleep(delay)
+try:
+	while True:
+	    clear()
+	    temp_raw = sensor.getTemp()
+	    temp = temp_raw
+	    if temp > 128:
+	        temp = temp - 256
+	    print temp
+	    write_string( "%.1f" % temp + " c", kerning=False)
+	    show()
+	    time.sleep(delay)
+
+except:
+	    write_string( "Exit", kerning=False)
+	    show()
